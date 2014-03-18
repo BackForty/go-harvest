f39ceac4ddc44c8f653d113a5e2e6617d3d69441
Ed Schmalzle <ed@nerded.net>
2014-03-18 14:41:50 -0400

    Fix formatting

--- harvest/api_client.go
+++ harvest/api_client.go
@@ -1,65 +1,67 @@
 package harvest
 
-import ("net/http"
-        "fmt"
-        "io/ioutil")
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 type APIClient struct {
-  username string
-  password string
-  token string
-  subdomain string
-  Client *http.Client
+	username  string
+	password  string
+	token     string
+	subdomain string
+	Client    *http.Client
 }
 
 func newAPIClient(subdomain string) (c *APIClient) {
-  c = new(APIClient)
-  c.subdomain = subdomain
-  c.Client = new(http.Client)
-  return
+	c = new(APIClient)
+	c.subdomain = subdomain
+	c.Client = new(http.Client)
+	return
 }
 
 func NewAPIClientWithBasicAuth(username, password, subdomain string) (c *APIClient) {
-  c = newAPIClient(subdomain)
-  c.username = username
-  c.password = password
-  return
+	c = newAPIClient(subdomain)
+	c.username = username
+	c.password = password
+	return
 }
 
 func NewAPIClientWithAuthToken(token, subdomain string) (c *APIClient) {
-  c = newAPIClient(subdomain)
-  c.token = token
-  return
+	c = newAPIClient(subdomain)
+	c.token = token
+	return
 }
 
-func (c *APIClient) GetJSON(path string) (jsonResponse []byte){
-  resourceURL := fmt.Sprintf("http://%v.harvestapp.com%v", c.subdomain, path)
-  request, err := http.NewRequest("GET", resourceURL, nil)
-  perror(err)
+func (c *APIClient) GetJSON(path string) (jsonResponse []byte) {
+	resourceURL := fmt.Sprintf("http://%v.harvestapp.com%v", c.subdomain, path)
+	request, err := http.NewRequest("GET", resourceURL, nil)
+	perror(err)
 
-  request.SetBasicAuth(c.username, c.password)
-  resp, err := c.Client.Do(request)
-  perror(err)
-  defer resp.Body.Close()
+	request.SetBasicAuth(c.username, c.password)
+	resp, err := c.Client.Do(request)
+	perror(err)
+	defer resp.Body.Close()
 
-  contents, err := ioutil.ReadAll(resp.Body)
-  perror(err)
+	contents, err := ioutil.ReadAll(resp.Body)
+	perror(err)
 
-  return contents
+	return contents
 }
 
 func (c *APIClient) GetClients() (clients []Client) {
-  clients = GetClients(c)
-  return
+	clients = GetClients(c)
+	return
 }
 
 func (c *APIClient) GetClient(clientID int32) (client Client) {
-  client = GetClient(clientID, c)
-  return
+	client = GetClient(clientID, c)
+	return
 }
 
 func perror(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }

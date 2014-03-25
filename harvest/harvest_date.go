b87251dc80370174d557c67fed46dca9734fc2f8
Ed Schmalzle <ed@nerded.net>
2014-03-25 10:56:59 -0400

    simplify harvest date unmarshal logic

--- harvest/harvest_date.go
+++ harvest/harvest_date.go
@@ -11,11 +11,10 @@ type HarvestDate struct {
 
 func (harvestDate *HarvestDate) UnmarshalJSON(input []byte) (err error) {
 	dateString := string(input)
-	if dateString == "null" {
 
-	} else if len(dateString) == 12 {
+	if len(dateString) == 12 {
 		harvestDate.Time, err = time.Parse("\"2006-01-02\"", dateString)
-	} else {
+	} else if dateString != "null" {
 		err = json.Unmarshal(input, &harvestDate.Time)
 	}
 
